3596a2d8379a77f109adc0f62a81fa02ecfdf3df
turkprogrammer <ostbat43@gmail.com>
2019-08-28 15:00:30 +0500

    Исключение для несуществутющего твоара

--- controllers/CatalogController.php
+++ controllers/CatalogController.php
@@ -32,33 +32,32 @@ class CatalogController extends AppController {
         return $this->render(
                         'category', compact('category', 'products', 'pages')
         );
-       
     }
 
     /**
      * Список всех брендов каталога товаров
      */
     public function actionBrands() {
-            
+
         // пробуем извлечь данные из кеша
         $brands = Yii::$app->cache->get('all-brands');
         if ($brands === false) {
             // данных нет в кеше, получаем их заново
             $brands = (new Brand())->getAllBrands();
             // сохраняем полученные данные в кеше
-            Yii::$app->cache->set('all-brands', $brands);        }
+            Yii::$app->cache->set('all-brands', $brands);
+        }
         $this->setMeta('Бренды');
         return $this->render(
-            'brands',
-            compact('brands')
+                        'brands', compact('brands')
         );
     }
 
     /**
      * Список товаров бренда с идентификатором $id
      */
-    public function actionBrand($id) {        
-        $id = (int)$id;
+    public function actionBrand($id) {
+        $id = (int) $id;
         $temp = new Brand();
         // товары бренда
         list($products, $pages) = $temp->getBrandProducts($id);
@@ -69,67 +68,69 @@ class CatalogController extends AppController {
         }
         // устанавливаем мета-теги
         $this->setMeta(
-            $brand->name . ' | ' . Yii::$app->params['shopName']
-          /*  $brand->keywords,
-            $brand->description*/
+                $brand->name . ' | ' . Yii::$app->params['shopName']
+                /*  $brand->keywords,
+                  $brand->description */
         );
         return $this->render(
-            'brand',
-            compact('brand', 'products', 'pages')
+                        'brand', compact('brand', 'products', 'pages')
         );
     }
+
     /**
      * Страница товара с идентификатором $id
      */
     public function actionProduct($id) {
-        $id = (int)$id;
+        $id = (int) $id;
         // пробуем извлечь данные из кеша
-        $data = Yii::$app->cache->get('product-'.$id);
+        $data = Yii::$app->cache->get('product-' . $id);
         if ($data === false) {
             // данных нет в кеше, получаем их заново
             $product = (new Product())->getProduct($id);
-            $brand = (new Brand())->getBrand($product['producer_id']);  
+            if (empty($product)) {
+                throw new \yii\web\HttpException(404, 'Товара не существует!');
+            }
+            $brand = (new Brand())->getBrand($product['producer_id']);
             $category = (new Category())->getCategory($product['cats']);
-            
+
             //self::debug($category); ;
-     
+
             $data = [$product, $brand];
             // сохраняем полученные данные в кеше
-            Yii::$app->cache->set('product-'.$id, $data);
+            Yii::$app->cache->set('product-' . $id, $data);
         }
         list($product, $brand) = $data;
         // устанавливаем мета-теги
         $this->setMeta(
-            $product['name'] . ' | ' . Yii::$app->params['shopName']
+                $product['name'] . ' | ' . Yii::$app->params['shopName']
         );
         // получаем  товары одного бренда, похожие на текущий
-        $similar = Yii::$app->cache->get('similar-'.$product['id']);
+        $similar = Yii::$app->cache->get('similar-' . $product['id']);
         if ($similar === false) {
             // товары из той же категории того же бренда
             $similar = Product::find()
-                ->where([
-                   /* 'hit' => 1,*/
-                    'cats' => $product['cats'],
-                    'producer_id' => $product['producer_id']
-                ])
-                ->andWhere(['NOT IN', 'id', $product['id']])
-                ->limit(3)
-                ->asArray()
-                ->all();
-            Yii::$app->cache->set('similar-'.$product['id'], $similar);
+                    ->where([
+                        /* 'hit' => 1, */
+                        'cats' => $product['cats'],
+                        'producer_id' => $product['producer_id']
+                    ])
+                    ->andWhere(['NOT IN', 'id', $product['id']])
+                    ->limit(3)
+                    ->asArray()
+                    ->all();
+            Yii::$app->cache->set('similar-' . $product['id'], $similar);
         }
         return $this->render(
-            'product',
-            compact('product', 'brand', 'similar', 'category')
+                        'product', compact('product', 'brand', 'similar', 'category')
         );
     }
-    
+
     /**
      * Результаты поиска по каталогу товаров
      */
     public function actionSearch($query = '', $page = 1) {
 
-        $page = (int)$page;
+        $page = (int) $page;
 
         // получаем результаты поиска с постраничной навигацией
         list($products, $pages) = (new Product())->getSearchResult($query, $page);
@@ -138,10 +139,8 @@ class CatalogController extends AppController {
         $this->setMeta('Поиск по каталогу');
 
         return $this->render(
-            'search',
-            compact('products', 'pages')
+                        'search', compact('products', 'pages')
         );
     }
- 
 
 }
